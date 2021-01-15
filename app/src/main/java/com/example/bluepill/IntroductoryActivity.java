4267f93077139ee04054ea6774fc5612035acf4e
Braimah-Abiola <braimahabiola.official@gmail.com>
2021-01-15 16:05:18 +0000

    Updates

--- app/src/main/java/com/example/bluepill/IntroductoryActivity.java
+++ app/src/main/java/com/example/bluepill/IntroductoryActivity.java
@@ -2,7 +2,10 @@ package com.example.bluepill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,16 +21,29 @@ public class IntroductoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_introductory);
         icon = findViewById(R.id.icon);
         logo = findViewById(R.id.logo);
         appName = findViewById(R.id.txt);
-       /* lottieAnimationView = findViewById(R.id.lottie); */
+        lottieAnimationView = findViewById(R.id.lottie);
 
-        logo.animate().translationY(1400).setDuration(1000).setStartDelay(2000);
-        appName.animate().translationY(1400).setDuration(1000).setStartDelay(2000);
-        icon.animate().translationY(1400).setDuration(1000).setStartDelay(2000);
-       // lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        icon.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(IntroductoryActivity.this, Login.class));
+            }
+        };
+
+        Handler h = new Handler();
+        h.postDelayed(r, 4000);
 
     }
+
+
 }
--- app/src/main/java/com/example/bluepill/Login.java
+++ app/src/main/java/com/example/bluepill/Login.java
@@ -3,12 +3,15 @@ package com.example.bluepill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
     }
 }
