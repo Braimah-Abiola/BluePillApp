b9eb0b911d8e4521a84078c8481357a6a8303c87
Braimah-Abiola <braimahabiola.official@gmail.com>
2021-01-16 04:10:00 +0000

    Cuberto

--- app/src/main/java/com/example/bluepill/IntroductoryActivity.java
+++ app/src/main/java/com/example/bluepill/IntroductoryActivity.java
@@ -1,6 +1,11 @@
 package com.example.bluepill;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,6 +23,10 @@ public class IntroductoryActivity extends AppCompatActivity {
     TextView appName;
     LottieAnimationView lottieAnimationView;
 
+    private static  final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +37,16 @@ public class IntroductoryActivity extends AppCompatActivity {
         appName = findViewById(R.id.txt);
         lottieAnimationView = findViewById(R.id.lottie);
 
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
         logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
         appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
         icon.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
 
-        Runnable r = new Runnable() {
+       /* Runnable r = new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(IntroductoryActivity.this, Login.class));
@@ -41,9 +54,38 @@ public class IntroductoryActivity extends AppCompatActivity {
         };
 
         Handler h = new Handler();
-        h.postDelayed(r, 4000);
+        h.postDelayed(r, 4000); */
 
     }
 
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+
+                case 0:
+                OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                return  tab1;
+                case 1:
+                OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                return  tab2;
+                case 2:
+                OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                return  tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/bluepill/OnBoardingFragment3.java
@@ -0,0 +1,20 @@
+package com.example.bluepill;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class OnBoardingFragment3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_on_boarding3, container, false);
+
+        return root;
+    }
+
+}
