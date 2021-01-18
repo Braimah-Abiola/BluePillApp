5ae42032c88c3e2cb08d9928d5478057cb2c04f7
Braimah-Abiola <braimahabiola.official@gmail.com>
2021-01-18 01:35:23 +0000

    Updates

---
+++ app/src/main/java/com/example/bluepill/OnBoarding/OnBoardingFragment2.java
@@ -0,0 +1,21 @@
+package com.example.bluepill.OnBoarding;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.bluepill.R;
+
+public class OnBoardingFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_on_boarding2, container, false);
+
+        return root;
+    }
+}
